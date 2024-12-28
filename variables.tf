fde1f8d9cfe7d2114099c458ccf4884865fa7eae
Shivakumar3218 <nattalashiva3218@gmail.com>
2024-12-28 14:52:03 +0000

    adding files

---
+++ variables.tf
@@ -0,0 +1,10 @@
+#variables
+variable "aws_access_key" {
+  type        = string
+  description = "enter the access key"
+}
+variable "aws_secreate_key" {
+  type        = string
+  description = "enter the secreate key"
+}
+
